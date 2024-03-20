fbba1972fab9d21ceb32a5a9e120c52bf4b37284
Ilia <72198091+umnikum@users.noreply.github.com>
2024-03-20 18:16:56 +0600

    updating riverland programm

--- 2023⁄2024 с++/Занятие №20/Riverland.cpp
+++ 2023⁄2024 с++/Занятие №20/Riverland.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -23,26 +26,104 @@ using namespace std;
  * и с высотами уровня воды для ключа.
  */
 
-int main() {
-	cout << "Enter location size:\n";
-	//Вводим размер локации
-	int n, amount;
-	cin >> n;
-	//Чтобы лишний раз не считать количество элементов заведём переменную
-	amount = n*n;
-	cout << "Enter location elements:\n";
-	//Вводим нужное количество элементов по очереди
-	vector<int> location(amount);
-	for(int i=0; i<amount; ++i)
-		cin >> location[i];
+class Riverland{
+public:
+	int size;
+	vector<int> heights;
 
-	//Теперь также выводим их
-	for(int i=0; i<amount; ++i){
-		//Пользуемся табуляцией \t
-		cout << location[i] << "\t";
-		//Не забываем переводить строку после n элементов(от 0 до n-1)
-		if(i%n == n-1)
-			cout << endl;
+
+	/*Riverland(const int &size, const vector<int> &heights){
+		this->size = size;
+		this->heights = heights;
+	}*/
+	Riverland(const int &size, const vector<int> &heights):
+		size(size), heights(heights){}
+	Riverland(const string &file_name):
+		size(0), heights(){
+		ifstream ifs{file_name};
+		ifs >> size;
+		int amount = size*size;
+		vector<int> location(amount);
+		for(int i=0; i<amount; ++i)
+			ifs >> location[i];
+		ifs.close();
+		heights = location;
 	}
+
+
+	bool is_boarder(const int &i, const int &j)const{
+		return (i==0 or i==size-1 or j==0 or j==size-1);
+	}
+
+
+	int count_index(const int &i, const int&j)const{
+		return i*size+j;
+	}
+
+	int count_x(const int &index)const{
+		return index%size;
+	}
+
+	int count_y(const int &index)const{
+		return index/size;
+	}
+
+	int search_shortest(const int &position)const{
+		int shortest=position, check_position=position-1;
+		shortest = (heights[shortest] >= heights[check_position])? check_position : shortest;
+		check_position=position+1;
+		shortest = (heights[shortest] >= heights[check_position])? check_position : shortest;
+		check_position=position-size;
+		shortest = (heights[shortest] >= heights[check_position])? check_position : shortest;
+		check_position=position+size;
+		shortest = (heights[shortest] >= heights[check_position])? check_position : shortest;
+		return shortest;
+	}
+
+
+	vector<int> find_exit(const int &x, const int &y)const{
+		vector<int> river;
+		int i=y, j=x;
+		river.push_back(count_index(i,j));
+		while(!is_boarder(i, j)){
+			int index = count_index(i,j),
+				next_index=search_shortest(index);
+			if(next_index != index){
+				i = count_y(next_index);
+				j = count_x(next_index);
+				river.push_back(next_index);
+			}else break;
+		}
+		return river;
+	}
+
+
+	string print()const{
+		stringstream ss;
+		int amount=size*size;
+		for(int i=0; i<amount; ++i){
+			ss << heights[i] << "\t";
+			if(i%size == size-1)
+				ss << "\n";
+		}
+		return ss.str();
+	}
+};
+
+int main() {
+	Riverland location("input.txt");
+	cout << location.print();
+
+	int x, y;
+	cout << "Enter starting coordinates:\n";
+	cin >> x >> y;
+
+	cout << "From: ";
+	auto river = location.find_exit(x, y);
+	for(const int &index:river){
+		cout << "(" << location.count_x(index) << ", "
+				<< location.count_y(index) << ") -to-> ";
+	}
+
 	return 0;
 }
