c2b9fe76c4702fcb09079f018a3df68d1c1ae9ad
Ilia <72198091+umnikum@users.noreply.github.com>
2024-11-27 21:00:37 +0600

    Реализация класса времени с бонусами

--- 2024⁄2025 c++/Занятие №09/Time.h
+++ 2024⁄2025 c++/Занятие №09/Time.h
@@ -2,19 +2,63 @@
 #define TIME_H_
 
 #include <string>
+#include <sstream>
 
 class Time{
+private:
+	void init(int total_seconds){
+		if(total_seconds < 0){
+			unsigned int day = HOURS_IN_DAY*MINUTES_IN_HOUR*SECONDS_IN_MINUTE;
+			total_seconds = day - (-total_seconds) % day
+		}
+		seconds = total_seconds % SECONDS_IN_MINUTE;
+		minutes = total_seconds / SECONDS_IN_MINUTE % MINUTES_IN_HOUR;
+		hours =  total_seconds / (SECONDS_IN_MINUTE * MINUTES_IN_HOUR) % HOURS_IN_DAY;
+	}
 public:
-	static constexpr int HOURS_IN_DAY = 24, MINUTES_IN_HOUR = 60, SECONDS_INT_MINUTE = 60;
-	int seconds, minutes, hours;
+	static constexpr unsigned int HOURS_IN_DAY = 24, MINUTES_IN_HOUR = 60, SECONDS_IN_MINUTE = 60;
+	unsigned int hours, minutes, seconds;
 	
-	Time(int seconds);
-	Time(int hours, int minutes, int seconds);
+	Time(int seconds){
+		init(seconds);
+	}
+	Time(unsigned int hours, unsigned int minutes, unsigned int seconds):
+		hours{hours}, minutes{minutes}, seconds{seconds}{
+		if(hours >= HOURS_IN_DAY or minutes >= MINUTES_IN_HOUR or seconds >= SECONDS_IN_MINUTE)
+			init(seconds + SECONDS_IN_MINUTE*minutes + SECONDS_IN_MINUTE*MINUTES_IN_HOUR*hours);
+	}
 	
-	Time operator+(const Time &time)const;
-	Time operator-(const Time &time)const;
+	int to_seconds()const{
+		return seconds + SECONDS_IN_MINUTE*minutes + SECONDS_IN_MINUTE*MINUTES_IN_HOUR*hours;
+	}
+	
+	Time operator+(const Time &time)const{
+		return Time{to_seconds() + time.to_seconds};
+	}
+	Time operator-(const Time &time)const{
+		return Time{to_seconds() - time.to_seconds};
+	}
 
-	friend std::string to_string(const Time &time);
+	bool operator==(const Time &time)const{
+		return hours==time.hours and minutes==time.minutes and seconds==time.seconds;
+	}
+	bool operator<(const Time &time)const{
+		return to_seconds() < time.to_seconds();
+	}
+	bool operator<=(const Time &time)const{
+		return to_seconds() <= time.to_seconds();
+	}
+	bool operator>(const Time &time)const{
+		return to_seconds() > time.to_seconds();
+	}
+	bool operator>=(const Time &time)const{
+		return to_seconds() >= time.to_seconds();
+	}
+	friend std::string to_string(const Time &time){
+		std::stringstream ss;
+		ss << hours << ":" << minutes << " : " << seconds;
+		return ss.str();
+	}
 };
 
 #endif /* TIME_H_ */
